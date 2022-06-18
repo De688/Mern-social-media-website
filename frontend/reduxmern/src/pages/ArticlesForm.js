01fe447e5c07e9ce49d883050fea2ff8671adc58
unknown <dedaniel688@gmail.com>
2022-06-18 02:57:12 -0700

    first commit

--- frontend/reduxmern/src/pages/ArticlesForm.js
+++ frontend/reduxmern/src/pages/ArticlesForm.js
@@ -1,107 +1,142 @@
-import React,{useState,useEffect} from 'react'
-import {toast} from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
-import {useDispatch,useSelector} from 'react-redux'
-import './articleForm.css'
-import {reset,updatearticle, createarticle} from '../redux/articleSlice.js'
-import LoadingSpinner from '../components/Loading/LoadingSpinner'
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useDispatch, useSelector } from "react-redux";
+import "./articleForm.css";
+import { reset, updatearticle, createarticle } from "../redux/articleSlice.js";
+import LoadingSpinner from "../components/Loading/LoadingSpinner";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
+function ArticlesForm({ id }) {
+  const [title, settitle] = useState("");
+  const [location, setlocation] = useState("");
+  const [description, setdescription] = useState("");
+  const [tags, settags] = useState("");
+  const [file, setfile] = useState("");
 
-function ArticlesForm({id}) {
+  //initialize redux imports
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { Articles, isSuccess, isLoading, isError, message, ArticleId } =
+    useSelector((state) => state.article);
 
-  const [title, settitle] = useState('')
-  const [location, setlocation] = useState('')
-  const [description, setdescription] = useState('')
-  const [tags, settags] = useState('')
-  const [file, setfile] = useState('')
-   
+  //useEffect
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
 
-     //initialize redux imports
-const dispatch = useDispatch()
-const navigate = useNavigate()
-const {Articles,isSuccess,isLoading,isError,message,ArticleId} = useSelector((state)=>state.article)
+    if (id) {
+      const singlearticle = Articles.find((article) => article._id === id);
 
+      settitle(singlearticle.title);
+      setlocation(singlearticle.location);
+      setdescription(singlearticle.description);
+      settags(singlearticle.tags);
+      setfile(`http://localhost:5000/images/${singlearticle.articleImage}`);
+    }
 
-     //useEffect
-     useEffect(() => {
-       if(isError){
-        toast.error(message)
-       }
-       
-       if(id){
-        const singlearticle = Articles.find((article)=>article._id === id)
+    dispatch(reset);
+  }, [ArticleId, Articles, isError, message, dispatch]);
 
-        settitle(singlearticle.title)
-        setlocation(singlearticle.location)
-        setdescription(singlearticle.description)
-        settags(singlearticle.tags)
-        setfile(`http://localhost:5000/images/${singlearticle.articleImage}`)
-       }
-      
-       dispatch(reset)
-     }, [ArticleId,Articles, isError,message,dispatch])
-     
-     
-  const submitdetails = async(e)=>{
-    e.preventDefault()
+  const submitdetails = async (e) => {
+    e.preventDefault();
     const articleData = {
-        title,location,description,tags,
-    }
-    if(file){
+      title,
+      location,
+      description,
+      tags,
+    };
+    if (file) {
       const data = new FormData();
-      const filename =  Date.now() + file.name;
-      data.append('name',filename)
-      data.append('file',file)
-      articleData.articleImage = filename
-     try {
-       await axios.post('http://localhost:5000/api/upload', data)
-     } catch (error) {
-       console.log(error.messsage.text)
-     }
-     
+      const filename = Date.now() + file.name;
+      data.append("name", filename);
+      data.append("file", file);
+      articleData.articleImage = filename;
+      try {
+        await axios.post("http://localhost:5000/api/upload", data);
+      } catch (error) {
+        console.log(error.messsage.text);
+      }
     }
-    if(id){
-        
-      const response =await axios.put('http://localhost:5000/api/articles/updatearticle/'+ id ,articleData)
-      console.log(response.data)
-      navigate('/')
+    if (id) {
+      const response = await axios.put(
+        "http://localhost:5000/api/articles/updatearticle/" + id,
+        articleData
+      );
+      console.log(response.data);
+      navigate("/");
       //dispatch(updatearticle(articleData,ArticleId))
-    }else{
-      dispatch(createarticle(articleData))
-      console.log(articleData)
+    } else {
+      dispatch(createarticle(articleData));
+      console.log(articleData);
     }
-  }
-  const clear=()=>{
+  };
+  const clear = () => {};
+  const onsubmit = (e) => {
+    e.preventDefault();
+  };
 
-  }
-  const onsubmit = (e) =>{
-    e.preventDefault()
-  }
-  
-  if(isLoading){
-      return(<LoadingSpinner/>)
+  if (isLoading) {
+    return <LoadingSpinner />;
   }
   return (
-    <section className='header2'>
-    <h4 className='textlogo'>Dashboard</h4>
-    <form onSubmit={onsubmit} className='info'>
-        <input type='text' value={title} onChange={(e)=>settitle(e.target.value)} className='inputbox' name='title' placeholder='enter title'/>
-        <input type='text' value={location} onChange={(e)=>setlocation(e.target.value)} className='inputbox' name='location' placeholder='enter location'/>
-        <input type='textarea' value={description} onChange={(e)=>setdescription(e.target.value)} className='inputbox' name='description' placeholder='enter description'/>
-        <input type='array' value={tags} onChange={(e)=>settags(e.target.value)} className='inputbox' name='tags' placeholder='enter tags'/>
-        <input type='file' maltiple='false' onChange={(e)=>setfile(e.target.files[0])} />
-        {ArticleId ?  <button type='submit' className='submitbtn' onClick={submitdetails}>Edit article</button>
- :
-          <button type='submit' className='submitbtn' onClick={submitdetails}>create article</button>
-
-        }
-        <button type='button' onClick={clear} className='clearbtn'>clear</button>
-
-    </form>
-  </section>
-  )
+    <section className="header2">
+      <h4 className="textlogo">Dashboard</h4>
+      <form onSubmit={onsubmit} className="info">
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => settitle(e.target.value)}
+          className="inputbox"
+          name="title"
+          placeholder="enter title"
+        />
+        <input
+          type="text"
+          value={location}
+          onChange={(e) => setlocation(e.target.value)}
+          className="inputbox"
+          name="location"
+          placeholder="enter location"
+        />
+        <input
+          type="textarea"
+          value={description}
+          onChange={(e) => setdescription(e.target.value)}
+          className="inputbox"
+          name="description"
+          placeholder="enter description"
+        />
+        <input
+          type="array"
+          value={tags}
+          onChange={(e) => settags(e.target.value)}
+          className="inputbox"
+          name="tags"
+          placeholder="enter tags"
+        />
+        <input
+          type="file"
+          maltiple="false"
+          onChange={(e) => setfile(e.target.files[0])}
+        />
+        {ArticleId ? (
+          <button type="submit" className="submitbtn" onClick={submitdetails}>
+            Edit article
+          </button>
+        ) : (
+          <button type="submit" className="submitbtn" onClick={submitdetails}>
+            create article
+          </button>
+        )}
+        <button type="button" onClick={clear} className="clearbtn">
+          clear
+        </button>
+      </form>
+    </section>
+  );
 }
 
-export default ArticlesForm
+export default ArticlesForm;
--- frontend/reduxmern/src/pages/Home.js
+++ frontend/reduxmern/src/pages/Home.js
@@ -1,93 +1,147 @@
-import React,{useEffect,useState} from 'react'
-import { FaThumbsUp,FaEllipsisH } from 'react-icons/fa';
-import {MdDelete} from 'react-icons/md'
-import {Link, useNavigate} from 'react-router-dom'
-import {useDispatch,useSelector} from 'react-redux'
-import {reset,Getarticles,addarticleid,deletearticle,likearticle} from '../redux/articleSlice.js'
-import {toast} from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
-import LoadingSpinner from '../components/Loading/LoadingSpinner';
-import { useParams } from 'react-router';
-import './home.css'
-import axios from 'axios'
-import moment from 'moment'
+import React, { useEffect, useState } from "react";
+import { FaThumbsUp, FaEllipsisH } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  reset,
+  Getarticles,
+  addarticleid,
+  deletearticle,
+  likearticle,
+} from "../redux/articleSlice.js";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import LoadingSpinner from "../components/Loading/LoadingSpinner";
+import { useParams } from "react-router";
+import "./home.css";
+import axios from "axios";
+import moment from "moment";
 
-function Home({setid}) {
-  const PublicFilder = 'http://localhost:5000/images/'
+function Home({ setid }) {
+  const PublicFilder = "http://localhost:5000/images/";
 
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const params = useParams()
-  const {user} = useSelector((state)=>state.auth)
-  const {Articles,isLoading,isError,isSuccess,message,} = useSelector((state)=>state.article)
- 
-  const showsinglearticle = (id) =>{
-    setid(id)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const params = useParams();
+  const { user } = useSelector((state) => state.auth);
+  const { Articles, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.article
+  );
 
-}
+  const showsinglearticle = (id) => {
+    setid(id);
+  };
 
+  const Deletearticle = (id) => {
+    dispatch(deletearticle(id));
+  };
+  const likedarticle = async (id) => {
+    const response = await axios.put(
+      `http://localhost:5000/api/articles/like/${id}`
+    );
 
-  const Deletearticle=(id)=>{
-    dispatch(deletearticle(id))
+    Articles.map((item) =>
+      item._id === response.data._id ? response.data : null
+    );
+    return response.data;
+  };
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    dispatch(Getarticles());
+    return () => {
+      dispatch(reset);
+    };
+  }, [user, isError, message, dispatch, navigate]);
+
+  if (isLoading) {
+    return <LoadingSpinner />;
   }
-  const likedarticle = async(id)=>{ 
-      const response = await axios.put(`http://localhost:5000/api/articles/like/${id}`)
-      
-      Articles.map((item)=>item._id === response.data._id ? response.data : null)
-      return response.data
+  if (Articles.length === 0) {
+    return <h4 className="nocontent">No contents availabe!</h4>;
   }
-
-useEffect(() => {
-  if(isError){
-    toast.error(message)
-  }
-  
-  dispatch(Getarticles())
-  return () => {dispatch(reset)}
-  
-}, [user,isError,message,dispatch,navigate])
-
-
-if(isLoading){
-  return(<LoadingSpinner/>)
-}
-if(Articles.length === 0){return(<h4 className='nocontent'>No contents availabe!</h4>)}
   return (
     <>
-    <div className='maindiv'>
-      <section className='container'>
-              {Articles.map((article, key)=>{
-                
-                const {title,location,description,tags,articleImage,likecount,_id,date} = article
-                
-                return(
-                <div className='card-main-div' key={_id} >
-                    <div className='imagePart'>
-                      <img className='articleimage' src={PublicFilder + articleImage} alt='articleimage'/>
-                      <div className='article-name'>
-                          <div className='titleandtime'>{title}<div className='dateonly'>{moment(date).startOf('day').fromNow()}</div></div>
-                          <div onClick={()=>dispatch(addarticleid(_id),navigate('/articleForm'), setid(_id))}><FaEllipsisH className='threedots'/></div>
+      <div className="maindiv">
+        <section className="container">
+          {Articles.map((article, key) => {
+            const {
+              title,
+              location,
+              description,
+              tags,
+              articleImage,
+              likecount,
+              _id,
+              date,
+            } = article;
+
+            return (
+              <div className="card-main-div" key={_id}>
+                <div className="imagePart">
+                  <img
+                    className="articleimage"
+                    src={PublicFilder + articleImage}
+                    alt="articleimage"
+                  />
+                  <div className="article-name">
+                    <div className="titleandtime">
+                      {title}
+                      <div className="dateonly">
+                        {moment(date).startOf("day").fromNow()}
                       </div>
                     </div>
-                     <div className='belowimgcontainer'>
-                        <div className='location'>{location}</div>
-                        <p key={key}>{` ${description.substring(0,100)}...`}
-                            <Link to={'/singlearticle'}>
-                                <button key={key}  className='btn' onClick={()=>showsinglearticle(_id)}>readmore</button>
-                            </Link>
-                        </p>
-                        <div className='likedeleteicons'>
-                          <div className='likentn' onClick={()=>likedarticle(_id)}><FaThumbsUp className='likentnicon'/>LIKE<span>{likecount}</span></div>
-                          <div className='deletebtn' onClick={()=>Deletearticle(_id)}><MdDelete/> DELETE</div>
-                        </div>
-                     </div>
+                    <div
+                      onClick={() =>
+                        dispatch(
+                          addarticleid(_id),
+                          navigate("/articleForm"),
+                          setid(_id)
+                        )
+                      }
+                    >
+                      <FaEllipsisH className="threedots" />
+                    </div>
+                  </div>
                 </div>
-                )
-              })}
-      </section>
+                <div className="belowimgcontainer">
+                  <div className="location">{location}</div>
+                  <p key={key}>
+                    {` ${description.substring(0, 100)}...`}
+                    <Link to={"/singlearticle"}>
+                      <button
+                        key={key}
+                        className="btn"
+                        onClick={() => showsinglearticle(_id)}
+                      >
+                        readmore
+                      </button>
+                    </Link>
+                  </p>
+                  <div className="likedeleteicons">
+                    <div className="likentn" onClick={() => likedarticle(_id)}>
+                      <FaThumbsUp className="likentnicon" />
+                      LIKE<span>{likecount}</span>
+                    </div>
+                    <div
+                      className="deletebtn"
+                      onClick={() => Deletearticle(_id)}
+                    >
+                      <MdDelete /> DELETE
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </section>
       </div>
-  </>
-  )
+    </>
+  );
 }
 
-export default Home
+export default Home;
